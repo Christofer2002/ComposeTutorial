7a4b5be3125ce1d7397423a0dc3a52259d7b6822
Christofer Daniel Chaves Pauth <cristoferchaves36@gmail.com>
2024-08-24 17:23:03 -0600

    Practice initialized

---
+++ README.md
@@ -0,0 +1,2 @@
+# ComposeTutorial
+ A simple tutorial project for learning the basics of Jetpack Compose with Kotlin. This repository includes examples of UI components, state management, and common patterns in modern Android development.
---
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.composetutorial
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.composetutorial.ui.theme.ComposeTutorialTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MessageCard(Message("Chris", "Check my Github!"))
+        }
+    }
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message){
+    Row(modifier = Modifier.padding(all = 9.dp)){
+        Image(
+            painter = painterResource(id = R.drawable._722203598965),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                // Set image size to 40 dp
+                .size(40.dp)
+                // Clip image to be shaped as a circle
+                .clip(CircleShape)
+        )
+        // Add a horizontal space between the image and the column
+        Spacer(modifier = Modifier.width(8.dp))
+        Column {
+            Text(text = "Hello ${msg.author}")
+            // Add a vertical space between the author and message texts
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = msg.body)
+        }
+    }
+
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard(){
+    MessageCard(msg = Message("Colleague", "Check out Jetpack Compose"))
+}
